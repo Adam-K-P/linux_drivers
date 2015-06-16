0b9cef25237867490b59a3f400cab8cadaee7b83
Adam-K-P <akpinarb@ucsc.edu>
2015-06-16 16:50:54 -0700

    worked on module

--- hello.c
+++ hello.c
@@ -3,11 +3,19 @@
 #include <linux/sched.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+static char *whom = "world";
+static int howmany = 1;
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
+
+
 static int __init hello_init(void)
 {
-        printk(KERN_ALERT "Hello, world\n");
-        printk(KERN_ALERT "the process is \"%s\" (pid %i)\n", 
-                           current->comm, current->pid);
+        for(; howmany > 0; --howmany) {
+                printk(KERN_ALERT "Hello, %s\n", whom);
+                printk(KERN_ALERT "the process is \"%s\" (pid %i)\n", 
+                                   current->comm, current->pid);
+        }
         return 0;
 }
 
